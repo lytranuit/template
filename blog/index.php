b2a480d3a4f90be83457b7839feecdd9d0f9ac92
DESKTOP-073RIC6\daotran <lytranuit@gmail.com>
2018-01-30 21:11:57 +0700

    init

--- blog/index.php
+++ blog/index.php
@@ -3,5 +3,7 @@ $dirs = array_filter(glob('*'), 'is_dir');
 $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
 <?php foreach ($dirs as $key => $row): ?>
-    <a href="<?= $actual_link . $row ?>">Category<?= $key ?></a>
+    <div>
+        <a href="<?= $actual_link . $row ?>"><?= $row ?></a>
+    </div>
 <?php endforeach; ?>
